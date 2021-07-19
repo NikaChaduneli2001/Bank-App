c62c8a1bd9526c2f494837f8832f1d488a82454f
Nika_Chaduneli <nika.chaduneli504@ens.tsu.edu.ge>
2021-07-19 17:37:03 +0400

    Update users.interface.ts

--- bank-app/src/interface/users.interface.ts
+++ bank-app/src/interface/users.interface.ts
@@ -1,9 +1,12 @@
+import { Role } from 'src/enums/role.enum';
+
 export class usersInterface {
   id: number;
   fullName: string;
   email: string;
   time: string;
   hash: string;
+  role: Role;
   phone: number;
   personalNumber: number;
   deleted: boolean;
