a8fe342393fc35a4f3a880f6e0b9b9d551dc3233
Nika_Chaduneli <nika.chaduneli504@ens.tsu.edu.ge>
2021-07-29 02:38:51 +0400

    Update serv.module.ts

--- bank-app/src/modules/serv/serv.module.ts
+++ bank-app/src/modules/serv/serv.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AccountMysqlsModule } from '../repositories/accounts/accounts_mysql.module';
-import { AccountsMysqlService } from '../repositories/accounts/accounts_mysql.repository';
 import { ServMySqlModule } from '../repositories/serv/service_mysql.module';
 import { ServiceController } from './serv.controller';
 import { ServiceService } from './serv.service';
