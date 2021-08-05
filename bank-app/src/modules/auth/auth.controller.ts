fe72fc2f5334ca129a9e116208638dac64181cda
Nika_Chaduneli <nika.chaduneli504@ens.tsu.edu.ge>
2021-08-05 15:10:48 +0400

    Update auth.controller.ts

--- bank-app/src/modules/auth/auth.controller.ts
+++ bank-app/src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { loginDto } from 'src/dto/login-users.dto';
 import { getErrorMessage } from 'src/utils/response-functions.utils';
 import { UsersService } from '../users/users.service';
@@ -6,6 +6,7 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  private readonly logger = new Logger(AuthController.name);
   constructor(
     private readonly usersService: UsersService,
     private readonly authService: AuthService,
@@ -18,8 +19,10 @@ export class AuthController {
         data.email,
         data.password,
       );
+      this.logger.log(`found user ${JSON.stringify(foundUser)}`);
       if (foundUser) {
         const jwtToken = await this.authService.loginUser(foundUser);
+        this.logger.log(`users token: ${JSON.stringify(jwtToken)}`);
         return {
           status: 'success',
           data: {
@@ -29,6 +32,8 @@ export class AuthController {
             jwtToken,
           },
         };
+      } else {
+        return getErrorMessage('user was not found');
       }
     } catch {
       return getErrorMessage('Password or Email is incorrect');
